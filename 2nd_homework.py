5aad8abf1822876ca894f732f5fbd221ccf7b687
Darik576 <artemdarmonuk@gmail.com>
2026-02-08 14:28:45 +0200

    2 готове

---
+++ 2nd_homework.py
@@ -0,0 +1,23 @@
+import random
+
+def get_numbers_ticket(min_number, max_number, quantity):
+    if min_number < 1 or max_number > 1000:
+        return []
+
+    if quantity > (max_number - min_number + 1):
+        return []
+
+    numbers = []
+
+    while len(numbers) < quantity:
+        number = random.randint(min_number, max_number)
+
+        if number not in numbers:
+            numbers.append(number)
+
+    numbers.sort()
+    return numbers
+
+
+ticket = get_numbers_ticket(1, 49, 6)
+print("Ваші лотерейні числа:", ticket)
