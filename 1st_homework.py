a65824553779af0b935dcb1965b90241ee715a55
Darik576 <artemdarmonuk@gmail.com>
2026-02-09 20:24:29 +0200

    fix

--- 1st_homework.py
+++ 1st_homework.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+
 def get_days_from_today(date: str) -> int:
     try:
         given_date = datetime.strptime(date, "%Y-%m-%d").date()
@@ -14,10 +15,9 @@ def get_days_from_today(date: str) -> int:
 some_date = str(input(f"Введіть дату в форматі: РРРР-ММ-ДД: "))
 result = get_days_from_today(some_date)
 
+
 if result > 0:
     print(f"Від вказаної вами дати до сьогодні: {result} днів")
 else:
     result = result*-1
     print (f"Вказана вами дата настане через {result} днів")
-
-
--- 3rd_homework.py
+++ 3rd_homework.py
@@ -1,5 +1,6 @@
 import re
 
+
 def normalize_phone(phone_number):
     cleaned_number = re.sub(r"[^\d+]", "", phone_number)
 
@@ -12,18 +13,20 @@ def normalize_phone(phone_number):
     return "+38" + cleaned_number
 
 
-phone_number = [
-"    +38(050)123-32-34",
-"     0503451234",
-"(050)8889900",
-"38050-111-22-22",
-"38050 111 22 11   "
-]
+if __name__ == "__main__":
+    phone_number = [
+        "    +38(050)123-32-34",
+        "     0503451234",
+        "(050)8889900",
+        "38050-111-22-22",
+        "38050 111 22 11   ",
+        "09546225564"
+    ]
 
-sanitized_numbers = []
+    sanitized_numbers = []
 
-for number in phone_number:
-    sanitized_numbers.append(normalize_phone(number))
+    for number in phone_number:
+        sanitized_numbers.append(normalize_phone(number))
 
-print("Нормалізовані номери телефонів для SMS-розсилки:")
-print(sanitized_numbers)
+    print("Нормалізовані номери телефонів для SMS-розсилки:")
+    print(sanitized_numbers)
